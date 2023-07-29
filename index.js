340f0b72b1d62656846d015e4fb262242a683744
unknown <lucaspio.galvao@gmail.com>
2023-07-29 00:16:32 -0300

    Creation of the modularized calculator

---
+++ index.js
@@ -0,0 +1,19 @@
+import soma from "./somarModule.js"
+import subtrai from "./subtrairModule.js"
+import multiplica from "./multiplicarModule.js"
+import dividi from "./dividirModule.js"
+
+const x = parseInt(prompt('valor de x'))
+const y = parseInt(prompt('valor de y'))
+
+console.log(
+
+soma(x, y),
+
+subtrai(x, y),
+
+multiplica(x, y),
+
+dividi(x, y)
+
+)
